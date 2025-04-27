8b914be7e5faf8de8c1a18c665ec81bb49381d22
TeodorUzunov <uzunovteodor@gmail.com>
2025-04-27 23:01:59 +0200

    Minor composer changes

--- app/settings.php
+++ app/settings.php
@@ -17,7 +17,7 @@ return function (ContainerBuilder $containerBuilder) {
                 'logError'            => false,
                 'logErrorDetails'     => false,
                 'logger' => [
-                    'name' => 'simple-chat-api',
+                    'name' => 'simple-chat-app-bunq',
                     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                     'level' => Logger::DEBUG,
                 ],
